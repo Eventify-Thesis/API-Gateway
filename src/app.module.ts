5a41cea41864c2b7d07309214fe2b592cb1209d3
git personal <buinguyenhoang0103@gmail.com>
2025-05-12 13:39:41 +0700

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,7 +43,7 @@ import { SearchController } from './search/search.controller';
     SeatController,
     SSEEventsController,
     EventController,
-    OrderController
+    OrderController,
     SearchController,
   ],
   providers: [
